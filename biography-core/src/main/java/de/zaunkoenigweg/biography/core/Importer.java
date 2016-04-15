93c4f2688d33339db47309cf30d0121cd2b69924
backstreetkiwi <mail@nikolaus-winter.de>
2016-04-15 17:02:54 +0200

    log4j

--- biography-core/src/main/java/de/zaunkoenigweg/biography/core/Importer.java
+++ biography-core/src/main/java/de/zaunkoenigweg/biography/core/Importer.java
@@ -3,25 +3,35 @@ package de.zaunkoenigweg.biography.core;
 import java.io.File;
 import java.io.FileFilter;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Importer {
+
+    private static final Logger LOG = LogManager.getLogger(Importer.class);
 
     private File importFolder;
     private File archive;
     
     private boolean isInitialized() {
         if(this.importFolder==null) {
+            LOG.trace("importFolder is null");
             return false;
         }
         if(this.archive==null) {
+            LOG.trace("archive folder is null");
             return false;
         }
         if(!(this.importFolder.exists() && this.importFolder.isDirectory())) {
+            LOG.trace(String.format("importFolder does not exist or is not a folder: %s", importFolder.getAbsolutePath()));
             return false;
         }
         if(!(this.archive.exists() && this.archive.isDirectory())) {
+            LOG.trace(String.format("archive folder does not exist or is not a folder: %s", archive.getAbsolutePath()));
             return false;
         }
         if(this.archive.equals(this.importFolder)) {
+            LOG.trace("importFolder must not be the same as archive folder");
             return false;
         }
         return true;
@@ -44,8 +54,10 @@ public class Importer {
                 return false;
             }
         });
+        File imageFile = null;
         for (int i = 0; i < imageFiles.length; i++) {
-            // TODO import
+            imageFile = imageFiles[i];
+            LOG.trace(String.format("Importing file %s", imageFile.getAbsolutePath()));
         }
         
     }
--- biography-core/src/main/java/de/zaunkoenigweg/biography/core/Main.java
+++ biography-core/src/main/java/de/zaunkoenigweg/biography/core/Main.java
@@ -2,15 +2,22 @@ package de.zaunkoenigweg.biography.core;
 
 import java.io.File;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    
+    private static final Logger LOG = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
+        LOG.info("Biography importer started.");
         String propertyImportFolder = System.getProperty("biography.import.folder");
         String propertyArchiveFolder = System.getProperty("biography.archive.folder");
         Importer importer = new Importer();
         importer.setImportFolder(new File(propertyImportFolder));
         importer.setArchive(new File(propertyArchiveFolder));
         importer.importAll();
+        LOG.info("Biography importer finished.");
     }
 
 }
