841356c14c6627b978a55a0414a28eccbed00ee8
backstreetkiwi <mail@nikolaus-winter.de>
2016-12-16 07:05:12 +0100

    model for albums

--- biography-core/src/main/java/de/zaunkoenigweg/biography/metadata/Album.java
+++ biography-core/src/main/java/de/zaunkoenigweg/biography/metadata/Album.java
@@ -2,6 +2,8 @@ package de.zaunkoenigweg.biography.metadata;
 
 import java.util.Optional;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 
@@ -14,6 +16,9 @@ import com.google.gson.JsonSyntaxException;
  */
 public class Album {
 
+    public static final String SEPARATOR = "::";
+    
+    private String id;
     private String title;
     private String chapter;
     
@@ -22,33 +27,72 @@ public class Album {
     }
     
     public Album(String title) {
+        if(StringUtils.isBlank(title)) {
+            throw new IllegalArgumentException("Album title must not be blank.");
+        }
+        if(StringUtils.contains(title, SEPARATOR)) {
+            throw new IllegalArgumentException(String.format("Album title must not contain the separator String (\"%s\").", SEPARATOR));
+        }
+        this.id = title;
         this.title = title;
     }
 
     public Album(String title, String chapter) {
+        if(StringUtils.isBlank(title)) {
+            throw new IllegalArgumentException("Album title must not be blank.");
+        }
+        if(StringUtils.isBlank(chapter)) {
+            throw new IllegalArgumentException("Album chapter must not be blank.");
+        }
+        if(StringUtils.contains(chapter, SEPARATOR)) {
+            throw new IllegalArgumentException(String.format("Album chapter must not contain the separator String (\"%s\").", SEPARATOR));
+        }
+        if(StringUtils.contains(title, SEPARATOR)) {
+            throw new IllegalArgumentException(String.format("Album title must not contain the separator String (\"%s\").", SEPARATOR));
+        }
+        this.id = String.format("%s%s%s", title, SEPARATOR, chapter);
         this.title = title;
         this.chapter = chapter;
     }
 
     public String toJson() {
         Gson gson = new Gson();
-        return gson.toJson(this);
+        return gson.toJson(id);
     }
     
-    public static Album from(String json) {
+    public static Album fromJson(String json) {
         Gson gson = new Gson();
         try {
-            return gson.fromJson(json, Album.class);
+            String id = gson.fromJson(json, String.class);
+            return fromId(id);
         } catch (JsonSyntaxException e) {
-            return null;
+            throw new IllegalArgumentException("No valid Json String: " + json);
         }
     }
 
+    public String getId() {
+        return id;
+    }
+    
     public String getTitle() {
         return title;
     }
 
     public Optional<String> getChapter() {
         return Optional.ofNullable(this.chapter);
+    }
+    
+    private static Album fromId(String id) {
+        if(StringUtils.isBlank(id)) {
+            throw new IllegalArgumentException("Album id must not be blank.");
+        }
+        if(!StringUtils.contains(id, SEPARATOR)) {
+            return new Album(id);
+        }
+        String[] tokens = StringUtils.splitByWholeSeparator(id, SEPARATOR);
+        if(tokens.length!=2) {
+            throw new IllegalArgumentException("Album id is not valid.");
+        }
+        return new Album(tokens[0], tokens[1]);
     }
 }
--- biography-core/src/test/java/de/zaunkoenigweg/biography/metadata/AlbumTest.java
+++ biography-core/src/test/java/de/zaunkoenigweg/biography/metadata/AlbumTest.java
@@ -2,9 +2,8 @@ package de.zaunkoenigweg.biography.metadata;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
@@ -17,66 +16,108 @@ public class AlbumTest {
     public void setUp() throws IOException {
     }
 
+    @Test(expected=IllegalArgumentException.class)
+    public void testCreateTitleNull() {
+        new Album(null);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testCreateEmptyTitle() {
+        new Album("");
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testCreateBlankTitle() {
+        new Album("  ");
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testCreateTitleContainsSeparator() {
+        new Album("title" + Album.SEPARATOR + "something");
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testCreateChapterNull() {
+        new Album("title", null);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testCreateEmptyChapter() {
+        new Album("title", "");
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testCreateBlankChapter() {
+        new Album("title", "  ");
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testCreateChapterContainsSeparator() {
+        new Album("title", "something" + Album.SEPARATOR + "chapter");
+    }
+
     @Test
     public void testToJsonTitleOnly() {
         Album album = new Album("NZ 2005");
         String json = album.toJson();
-        assertEquals("{\"title\":\"NZ 2005\"}", json);
+        assertEquals("\"NZ 2005\"", json);
     }
 
     @Test
     public void testToJsonTitleAndChapter() {
         Album album = new Album("NZ 2005", "03 Kaiteriteri");
         String json = album.toJson();
-        assertEquals("{\"title\":\"NZ 2005\",\"chapter\":\"03 Kaiteriteri\"}", json);
+        assertEquals("\"NZ 2005" + Album.SEPARATOR + "03 Kaiteriteri\"", json);
     }
 
     @Test
     public void testFromJsonTitleOnly() {
-        String json = "{\"title\":\"NZ 2005\"}";
-        Album album = Album.from(json);
+        String json = "\"NZ 2005\"";
+        Album album = Album.fromJson(json);
         assertNotNull(album);
         assertNotNull(album.getTitle());
         assertNotNull(album.getChapter());
         assertFalse(album.getChapter().isPresent());
+        assertEquals("NZ 2005", album.getId());
         assertEquals("NZ 2005", album.getTitle());
     }
 
     @Test
     public void testFromJsonTitleAndChapter() {
-        String json = "{\"title\":\"NZ 2005\",\"chapter\":\"03 Kaiteriteri\"}";
-        Album album = Album.from(json);
+        String json = "\"NZ 2005" + Album.SEPARATOR + "03 Kaiteriteri\"";
+        Album album = Album.fromJson(json);
         assertNotNull(album);
         assertNotNull(album.getTitle());
         assertNotNull(album.getChapter());
         assertTrue(album.getChapter().isPresent());
+        assertEquals("NZ 2005" + Album.SEPARATOR + "03 Kaiteriteri", album.getId());
         assertEquals("NZ 2005", album.getTitle());
         assertEquals("03 Kaiteriteri", album.getChapter().get());
     }
 
-    @Test
+    @Test(expected=IllegalArgumentException.class)
+    public void testFromJsonMultipleSeparators() {
+        Album.fromJson("\"NZ 2005" + Album.SEPARATOR + "something" + Album.SEPARATOR + "03 Kaiteriteri\"");
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testFromJsonNoTitleBeforeSeparator() {
+        Album.fromJson("\"" + Album.SEPARATOR + "03 Kaiteriteri\"");
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testFromJsonNoChapterAfterSeparator() {
+        Album.fromJson("\"NZ 2005" + Album.SEPARATOR + "\"");
+    }
+
+    @Test(expected=IllegalArgumentException.class)
     public void testFromJsonEmptyJson() {
-        String json = "{}";
-        Album album = Album.from(json);
-        assertNotNull(album);
-        assertNull(album.getTitle());
-        assertNotNull(album.getChapter());
-        assertFalse(album.getChapter().isPresent());
-        assertNotNull(album);
+        Album.fromJson("{}");
     }
 
-    @Test
+    @Test(expected=IllegalArgumentException.class)
     public void testFromJsonEmptyString() {
-        String json = "";
-        Album album = Album.from(json);
-        assertNull(album);
-    }
-
-    @Test
-    public void testFromJsonBogusString() {
-        String json = "bogus";
-        Album album = Album.from(json);
-        assertNull(album);
+        Album.fromJson("");
     }
 
 }
