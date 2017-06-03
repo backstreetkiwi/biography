1762593c27c90b222e30046859ef80912c76890a
backstreetkiwi <mail@nikolaus-winter.de>
2017-06-03 11:34:04 +0200

    cleanup

--- biography-cli/src/main/java/de/zaunkoenigweg/biography/cli/MainDumpFileInfo.java
+++ biography-cli/src/main/java/de/zaunkoenigweg/biography/cli/MainDumpFileInfo.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
@@ -32,11 +30,8 @@ public class MainDumpFileInfo {
         
         BufferedReader stdInReader = new BufferedReader(new InputStreamReader(System.in));
         
-        String input = null;
-        
         try {
             System.out.println("Please enter full media file path(s): ");
-            List<File> mediaFilePaths = new ArrayList<>();
             String mediaFilePath = stdInReader.readLine();
             while(StringUtils.isNotBlank(mediaFilePath)) {
                 File file = new File(mediaFilePath);
