c8843d23b8cdd01be1e453325893c784d1cb21fa
Nikolaus Winter <nikolaus.winter@s24.com>
2018-01-20 23:52:43 +0100

    added SpringBoot web application

---
+++ biography-web/src/main/java/de/zaunkoenigweg/biography/web/Application.java
@@ -0,0 +1,41 @@
+package de.zaunkoenigweg.biography.web;
+
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }	
+    
+    @Bean
+    public File importFolder(@Value("${import.path}") String importPath, File archiveFolder) {
+    		File importFolder = new File(importPath);
+    		if(!importFolder.exists()) {
+    			String msg = String.format("The import folder %s does not exist", importPath);
+    			throw new RuntimeException(msg);
+    		}
+    		if(archiveFolder.equals(importFolder)) {
+    			String msg = String.format("The import folder (%s) must not be the same as the archive folder (%s)", importPath, archiveFolder.getAbsolutePath());
+    			throw new RuntimeException(msg);
+    		}
+    		return importFolder;
+    }
+
+    @Bean
+    public File archiveFolder(@Value("${archive.path}") String archivePath) {
+    		File archiveFolder = new File(archivePath);
+    		if(!archiveFolder.exists()) {
+    			String msg = String.format("The archive folder %s does not exist", archivePath);
+    			throw new RuntimeException(msg);
+    		}
+    		return archiveFolder;
+    }
+
+}
