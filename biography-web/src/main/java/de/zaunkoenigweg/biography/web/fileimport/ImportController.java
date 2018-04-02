07a28d92dcc58d5eab8ad7ab0ece5acbf31c5b26
Nikolaus Winter <nikolaus.winter@s24.com>
2018-04-02 19:14:26 +0200

    bulk import w/ zip support, page layout still creepy

--- biography-web/src/main/java/de/zaunkoenigweg/biography/web/fileimport/ImportController.java
+++ biography-web/src/main/java/de/zaunkoenigweg/biography/web/fileimport/ImportController.java
@@ -1,9 +1,14 @@
 package de.zaunkoenigweg.biography.web.fileimport;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
@@ -45,14 +50,68 @@ public class ImportController {
 
         File localFile = new File(importFolder, file.getOriginalFilename());
 
+        if (localFile.exists()) {
+            // TODO Proper error message
+            return "redirect:/alreadyexists";
+        }
+
         try {
             FileUtils.writeByteArrayToFile(localFile, file.getBytes(), false);
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+
+        if (StringUtils.equals(file.getContentType(), "application/zip")) {
+            unzip(localFile);
+            FileUtils.deleteQuietly(localFile);
+            this.archiveBulkImportService.clearImportFolder();
+            return "redirect:/import";
+        }
+
         this.archiveBulkImportService.clearImportFolder();
         return "redirect:/import";
+    }
+
+    // TODO: Do it better ;-)
+    // TODO: Handle existing files...
+    private void unzip(File zipFile) {
+        
+        byte[] buffer = new byte[1024];
+        
+        ZipInputStream zis = null;
+        try {
+            zis = new ZipInputStream(new FileInputStream(zipFile));
+            ZipEntry ze = zis.getNextEntry();
+            while (ze != null) {
+                if (ze.isDirectory() || ze.getName().startsWith(".") || ze.getName().startsWith("_")
+                        || ze.getName().contains("/")) {
+                    ze = zis.getNextEntry();
+                    continue;
+                }
+                File newFile = new File(importFolder + File.separator + ze.getName());
+
+                FileOutputStream fos = new FileOutputStream(newFile);
+
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+
+                ze = zis.getNextEntry();
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                zis.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 
     @PostMapping("/import/start")
