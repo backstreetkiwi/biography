200a984d7456d252a2772749983cb9b7fd2c5aca
Nikolaus Winter <mail@nikoaus-winter.de>
2018-08-16 15:52:46 +0200

    export album to disk

--- biography-web/src/main/java/de/zaunkoenigweg/biography/web/albums/AlbumsController.java
+++ biography-web/src/main/java/de/zaunkoenigweg/biography/web/albums/AlbumsController.java
@@ -1,12 +1,15 @@
 package de.zaunkoenigweg.biography.web.albums;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import de.zaunkoenigweg.biography.core.index.Album;
 import de.zaunkoenigweg.biography.core.index.ArchiveSearchService;
 import de.zaunkoenigweg.biography.core.index.MediaFile;
+import de.zaunkoenigweg.biography.core.util.BiographyFileUtils;
 import de.zaunkoenigweg.biography.web.BackLink;
 
 @Controller
@@ -59,6 +63,44 @@ public class AlbumsController {
         model.addAttribute("albums", albums);
         model.addAttribute("mediaFiles", mediaFiles);
         model.addAttribute("selectedMenuItem", "ALBUMS");
+        model.addAttribute("selectedAlbum", album);
+        
+        return "albums/index";
+    }
+    
+    @RequestMapping("/album/{album}/export")
+    public String exportAlbum(HttpSession session, HttpServletRequest request, Model model, @PathVariable("album") String album) {
+    	
+    	try {
+			File directory = new File(new File(archiveFolder, "albums"), album);
+			FileUtils.forceMkdir(directory);
+	        FileUtils.cleanDirectory(directory);
+	        archiveSearchService.findByAlbum(album).forEach(mf -> {
+	        	File mediaFile = BiographyFileUtils.getArchiveFileFromShortFilename(archiveFolder, mf.getFileName());
+	        	try {
+					FileUtils.copyFileToDirectory(mediaFile, directory);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	        });
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+    	
+        List<Album> albums = archiveSearchService.getAlbumCounts().collect(Collectors.toList());
+        
+        List<MediaFile> mediaFiles = archiveSearchService.findByAlbum(album).collect(Collectors.toList());
+
+        BackLink backLink = new BackLink("BACK TO ALBUM", request.getRequestURI());
+		session.setAttribute(BackLink.class.getName(), backLink);
+        
+        model.addAttribute("albums", albums);
+        model.addAttribute("mediaFiles", mediaFiles);
+        model.addAttribute("selectedMenuItem", "ALBUMS");
+        model.addAttribute("selectedAlbum", album);
         
         return "albums/index";
     }
