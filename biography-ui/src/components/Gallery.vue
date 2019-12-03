e378ef86f224c9d0d8c284bed7374b1d3d4db59c
backstreetkiwi <mail@nikolaus-winter.de>
2019-12-03 20:36:41 +0100

    [vue ui] edit description

--- biography-ui/src/components/Gallery.vue
+++ biography-ui/src/components/Gallery.vue
@@ -1,7 +1,10 @@
 <template>
     <div id="gallery">
         <div class="large-image" v-bind:class="{'large-image-hidden' : !showImage}" v-on:click="closeImagePopup()">
-            <img v-bind:src="this.imageSrc" />
+            <img v-bind:src="this.mediaFile!=null ? this.mediaFile.fileUrl: ''" />
+            <div class="description-overlay-large-image" v-bind:class="{'description-overlay-hidden' : !galleryShowDescription}">
+                <div class="description" v-on:click="editDescription()">{{this.mediaFile!=null ? this.mediaFile.description: ''}}</div>
+            </div>
         </div>
         <div class="gallery-toolbar">
             <GalleryToggleDescription :descriptionOn="galleryShowDescription" @toggled="descriptionToggled"/>
@@ -9,7 +12,7 @@
         </div>
         <div class="chapter" v-for="chapter in mediaFiles" v-bind:key="chapter.title">
             <div class="chapter-title">{{chapter.title}}</div>
-            <div class="gallery-item" v-for="mediaFile in chapter.mediaFiles" v-bind:key="mediaFile.fileName" v-on:click="showImagePopup(mediaFile.fileUrl)">
+            <div class="gallery-item" v-for="mediaFile in chapter.mediaFiles" v-bind:key="mediaFile.fileName" v-on:click="showImagePopup(mediaFile)">
                 <div class="gallery-thumbnail">
                     <img v-bind:src="mediaFile.thumbnailUrl"/>
                     <div class="description-overlay" v-bind:class="{'description-overlay-hidden' : !galleryShowDescription}">
@@ -37,6 +40,7 @@
         },
         data: function() {
             return {
+                baseUrl: "http://localhost:8080/",
                 galleryShowDescription: true,
                 galleryShowAlbums: false,
                 showImage: false
@@ -55,13 +59,25 @@
             albumsToggled: function(albumsOn) {
                 this.galleryShowAlbums = albumsOn;
             },
-            showImagePopup: function(fileName) {
-                this.imageSrc = fileName;
+            showImagePopup: function(mediaFile) {
+                this.mediaFile = mediaFile;
                 this.showImage = true;
             },
             closeImagePopup: function() {
                 this.showImage = false;
-                this.imageSrc = "";
+                this.mediaFile = null;
+            },
+            editDescription: function() {
+                var newDescription = prompt("new description", this.mediaFile.description);
+                if(newDescription) {
+                    var currentMediaFile = this.mediaFile;
+                    var restUrl = this.baseUrl + "rest/file/" + currentMediaFile.fileName + "?description=" + encodeURIComponent(newDescription);    
+                    axios({ method: "PUT", "url": restUrl }).then(result => {
+                        currentMediaFile.description = newDescription;
+                    }, error => {
+                        alert('Error')
+                    });
+                }
             }
         }
     }
@@ -142,15 +158,33 @@ div.description-overlay {
     padding: 0px;
 }
 
-div.description-overlay-hidden {
-    display: none;
-}
-
-div.description {
+div.description-overlay div.description {
     color: white;
     font-size: 15px;
     padding: 5px;
     text-align: center;
+}
+div.description-overlay-large-image {
+    position: absolute;
+    bottom: 0px;
+    left: 0px;
+    width: 100%;
+    height: auto;
+    overflow: hidden;
+    background-color: rgba(0,0,0,0.75);
+    z-index: 20;
+    cursor: pointer;    
+    padding: 0px;
+}
+
+div.description-overlay-large-image div.description {
+    color: white;
+    font-size: 20px;
+    padding: 10px;
+    text-align: center;
+}
+div.description-overlay-hidden {
+    display: none;
 }
 
 div.albums-overlay {
